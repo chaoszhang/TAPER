e221690b56275d064911d37cb4f3e6c0936d04a5
Chao Zhang <chz069@ucsd.edu>
2020-09-30 21:24:25 -0700

    Add files via upload

--- correction.jl
+++ correction.jl
@@ -1,4 +1,4 @@
-PROGRAM_VERSION = v"0.1.2-alpha"
+PROGRAM_VERSION = v"0.1.3-alpha"
 try
 	using ArgParse
 catch
@@ -8,7 +8,7 @@ catch
 end
 import Statistics.median
 
-function correction(fin, fout, k, X, MASK, pvalue, pseudocount, verbose)
+function correction(fin, fout, k, X, MASK, pvalue, qvalue, pseudocount, verbose)
 	inputText = read(fin, String)
 	temp = split(inputText, ">")
 	temp = temp[length.(temp) .> 0]
@@ -39,7 +39,7 @@ function correction(fin, fout, k, X, MASK, pvalue, pseudocount, verbose)
 		unq = length([1 for t in cnt if t > 0])
 		total = sum(cnt)
 		for j in 1:m
-			wo[i, j] = total == 0 ? 0 : (total + pseudocount) / (unq * cnt[UInt8(upperc[j][i])])
+			wo[i, j] = total == 0 ? 0 : (total + pseudocount) / (unq * cnt[UInt8(c[j][i])])
 		end
 	end
 	w1 = [wo[:,j][(arrc[j] .!= '-') .& (arrc[j] .!= X)] for j in 1:m]
@@ -49,9 +49,13 @@ function correction(fin, fout, k, X, MASK, pvalue, pseudocount, verbose)
 	wsum = [accumulate(+, arr) for arr in wsorted]
 	f(x, y, n, m) = x^2 / n + (n == m ? 0 : (y - x)^2 / (m - n))
 	var = [length(arr) > 0 ? f.(arr, arr[end], 1:length(arr), length(arr)) : [] for arr in wsum]
-	cutoffFloor = min([(i > (1 - pvalue) * length(var[j]) ? wsorted[j][i] : 3) for j in 1:m if length(var[j]) > 0 for i in [findmax(var[j])[2]]]...)
-	cutoffFloor = max(cutoffFloor, 1 + pseudocount * 5 / 12)
+	#cutoffFloor = min([(i > (1 - pvalue) * length(var[j]) ? wsorted[j][i] : 3) for j in 1:m if length(var[j]) > 0 for i in [findmax(var[j])[2]]]...)
+	#cutoffFloor = max(cutoffFloor, 1 + pseudocount * 5 / 12)
 	wCutoff = [length(var[j]) > 0 ? wsorted[j][findmax(var[j])[2]] : 0 for j in 1:m]
+	cutoffSorted = sort([wCutoff[j] for j in 1:m if length(var[j]) > 0])
+	println(stderr, cutoffSorted)
+	cutoffFloor = cutoffSorted[end - floor(Int, length(cutoffSorted) * pvalue)]
+	println(stderr, cutoffFloor)
 	s = zeros(n - k + 1, m, 2)
 	tiebreaker = zeros(n - k + 1, m, 2)
 	bt = zeros(Int64, n - k + 1, m, 2)
@@ -71,7 +75,7 @@ function correction(fin, fout, k, X, MASK, pvalue, pseudocount, verbose)
 		s = zeros(L, 2)
 		tiebreaker = zeros(L, 2)
 		bt = zeros(Int64, L, 2)
-		cutoff = max(wCutoff[j], cutoffFloor, 1)
+		cutoff = max(wCutoff[j], cutoffFloor, wsorted[j][end - floor(Int, length(wsorted[j]) * qvalue)])
 		for i in 1:L
 			v = (wj[i] > cutoff ? 0 : 1)
 			if i == 1
@@ -157,7 +161,7 @@ end
 function parse_commandline()
 	s = ArgParseSettings()
 	
-	@add_arg_table s begin
+	@add_arg_table! s begin
 		"--list", "-l"
 			help = "running on a list of inputs; for every two lines of the list file, the first one should be the path to the input and the second should be the path to its output"
 			action = :store_true
@@ -179,10 +183,14 @@ function parse_commandline()
 			help = "set k for k-mer"
 			arg_type = Int
 			default = 7
-		"--cutoff", "-c"
-			help = "set p-value cutoff to control the minimum aggressiveness of masking"
+		"--pCutoff", "-p"
+			help = "set p-value cutoff to control the aggressiveness of masking"
 			arg_type = Float64
-			default = 0.05
+			default = 0.2
+		"--qCutoff", "-q"
+			help = "set q-value cutoff to control the aggressiveness of masking"
+			arg_type = Float64
+			default = 0.2
 		"input"
 			help = "a fasta file as input (when -l is not set) or a list of input/output pairs (when -l is set)"
 			required = true
@@ -195,14 +203,14 @@ function main()
 	println(stderr, "Version " * string(PROGRAM_VERSION))
 	args = parse_commandline()
 	if args["list"] == false
-		correction(open(args["input"], "r"), stdout, args["k"], args["any"], args["mask"], 1 - args["cutoff"], args["nopseudocount"] ? 0 : 1, args["verbose"])
+		correction(open(args["input"], "r"), stdout, args["k"], args["any"], args["mask"], args["pCutoff"], args["qCutoff"], args["nopseudocount"] ? 0 : 1, args["verbose"])
 	else
 		temp = split(read(open(args["input"], "r"), String), "\n")
 		temp = temp[length.(temp) .> 0]
 		for i = 2:2:length(temp)
 			try
 				println(stderr, "Processing " * temp[i - 1] * "...")
-				correction(open(temp[i - 1], "r"), open(temp[i], "w"), args["k"], args["any"], args["mask"], 1 - args["cutoff"], args["nopseudocount"] ? 0 : 1, args["verbose"])
+				correction(open(temp[i - 1], "r"), open(temp[i], "w"), args["k"], args["any"], args["mask"], args["pCutoff"], args["qCutoff"], args["nopseudocount"] ? 0 : 1, args["verbose"])
 			catch
 				println(stderr, "Error happened when processing " * temp[i - 1] * ".")
 				println(stderr)
